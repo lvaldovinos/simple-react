c4c59dcfffe561ed18b9abdd1c29de19021113d6
Luis Valdovinos <lastkiss115@gmail.com>
2015-07-24 16:39:52 -0700

    Validate ssh key

--- api/router/api/index.js
+++ api/router/api/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
     router = express.Router();
     
 router.get('/', function(req, res, next) {
-  res.send('get apis');
+  res.send('get apis!');
 });
 
 router.post('/', function(req, res, next) {
